986f6725617c8bb20be3cd998e247ebf87ac104c
Gylerme0 <guilhermeoliveirasousa1369@gmail.com>
2025-11-22 14:11:26 -0300

    adicionado telas de ordens de separação

--- main.py
+++ main.py
@@ -48,3 +48,19 @@ async def movimentacoes_transferencia(request: Request):
 @app.get("/movimentacoes/ajuste")
 async def movimentacoes_ajuste(request: Request):
     return templates.TemplateResponse("movimentacoes/ajuste.html", {"request": request})
+
+@app.get("/ordens")
+async def ordens_index(request: Request):
+    return templates.TemplateResponse("ordens/index.html", {"request": request})
+
+@app.get("/ordens/monitor")
+async def ordens_monitor(request: Request):
+    return templates.TemplateResponse("ordens/monitor.html", {"request": request})
+
+@app.get("/ordens/criar")
+async def ordens_criar(request: Request):
+    return templates.TemplateResponse("ordens/criar.html", {"request": request})
+
+@app.get("/ordens/lista/{id}")
+async def ordens_lista(request: Request, id: str):
+    return templates.TemplateResponse("ordens/lista.html", {"request": request, "id": id})
